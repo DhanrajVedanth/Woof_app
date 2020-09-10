6ef00c6071cacd2f5099861bd77c577774e85c6d
Dhanraj Raghunathan <draghun@ncsu.edu>
2020-09-10 18:51:40 -0400

    unittests to be added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  def setup
+    @user = User.new(email: 'draghun@ncsu.edu', name: "Dhanraj", username: 'draghun')
+  end
+  # test "valid user" do
+  #   assert @user.valid?
+  # end
+
+  # test "invalid without email" do
+  #   @user.email = nil
+  #   refute @user.valid?,
+  #   assert_not_nil @user.errors[:email]
   # end
 end
